bdc82b6218f3e0bdd9906d517be9f2c7df17d308
duymt <duy.mtdevhust@gmail.com>
2025-01-10 15:46:48 +0700

    update

--- config/postgres.config.js
+++ config/postgres.config.js
@@ -12,18 +12,12 @@ const dev = {
 }
 
 const prod = {
-    host: process.env.PROD_POSTGRES_HOST || 'db-postgresql-sgp1-58474-do-user-15759758-0.f.db.ondigitalocean.com',
-    username: process.env.PROD_POSTGRES_USER || 'doadmin',
-    password: process.env.PROD_POSTGRES_PW || 'AVNS_b59T6z0uB8jwksrGte9',
-    database: process.env.PROD_POSTGRES_DB || 'defaultdb',
-    port: process.env.PROD_POSTGRES_PORT || '25060',
+    host: process.env.PROD_POSTGRES_HOST || 'localhost',
+    username: process.env.PROD_POSTGRES_USER || 'postgres',
+    password: process.env.PROD_POSTGRES_PW || 'postgres',
+    database: process.env.PROD_POSTGRES_DB || 'postgres',
+    port: process.env.PROD_POSTGRES_PORT || 5432,
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        }
-    }
 }
 
 
